e1e2eda0723a2fff226113242f011424f31838b5
DoLuongKhoa <43341150+DoLuongKhoa@users.noreply.github.com>
2018-11-21 22:00:05 +0700

    Add files via upload

--- bomberman/src/gin/oop/bomberman/entities/character/enemy/ai/AIMedium.java
+++ bomberman/src/gin/oop/bomberman/entities/character/enemy/ai/AIMedium.java
@@ -11,11 +11,48 @@ public class AIMedium extends AI {
 		_bomber = bomber;
 		_e = e;
 	}
-
+        public int sameCol(){
+            if(_bomber.getXTile() < _e.getXTile())
+			return 3;
+		else if(_bomber.getXTile() > _e.getXTile())
+			return 1;
+		
+		return -1;
+        }
+        public int sameRow(){
+            if(_bomber.getYTile() < _e.getYTile())
+			return 0;
+		else if(_bomber.getYTile() > _e.getYTile())
+			return 2;
+		return -1;
+        }
 	@Override
 	public int calculateDirection() {
 		// TODO: cài đặt thuật toán tìm đường đi
-		return 1;
+                if (_bomber == null) {
+                    return random.nextInt(4);
+                }
+                int pos = random.nextInt(2);
+                if (pos == 0) {
+                    int col = sameCol();
+			
+			if(col != -1) {
+                            return col;
+                        } else {
+                            return sameRow();
+                        }
+				
+			
+				
+                } else {
+                    int row = sameRow();
+                    if(row != - 1) {
+                        return row;
+                    } else {
+                        return sameCol();
+                    }
+                }
 	}
+        
 
 }
